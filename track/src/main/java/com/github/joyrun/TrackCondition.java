ef8543dd3067ca6ac36dd31f039d65ceebc61f2c
梁振星 <kid.world@163.com>
2016-09-25 17:10:50 +0800

    BindView apt

--- track/src/main/java/com/github/joyrun/TrackCondition.java
+++ track/src/main/java/com/github/joyrun/TrackCondition.java
@@ -9,18 +9,5 @@ import com.raizlabs.android.dbflow.structure.BaseModel;
  */
 public class TrackCondition extends BaseModel {
 
-    @PrimaryKey
-    String event;  //埋点的字段
 
-//    @Column
-//    String currentPagerName; //带包名的类名
-//
-//    @Column
-//    String fromPagerName;//进入前的pager
-//
-//    @Column
-//    int trigViewId;//触发这个行为的 空间的id
-
-    @Column
-    String conditionJson;
 }
--- track/src/main/java/com/github/joyrun/TrackHelper.java
+++ track/src/main/java/com/github/joyrun/TrackHelper.java
@@ -13,24 +13,23 @@ import com.raizlabs.android.dbflow.structure.database.transaction.Transaction;
 import java.util.ArrayList;
 import java.util.List;
 
+import hugo.weaving.DebugLog;
+
 /**
  * Created by keven on 16/9/22.
  */
 
 public class TrackHelper {
 
-    public static List<TrackCondition> trackConditionDatas = new ArrayList<>();
+    public static List<? extends TrackCondition> trackConditionDatas = new ArrayList<>();
     private static volatile boolean sIsInit = false;
 
     public static void initAptTrack(Context context){
-        FlowManager.init(context);
-
-        for (TrackCondition trackCondition : trackConditionDatas){
-
-        }
+        init(context);
+        saveModelTransaction(trackConditionDatas);
     }
 
-    public static void saveModelTransaction(final List<? extends Model> models) {
+    public static void saveModelTransaction(final List<? extends TrackCondition> models) {
         if (models == null || models.size() == 0) {
             return;
         }
@@ -51,8 +50,56 @@ public class TrackHelper {
         transaction.execute();
     }
 
+
+    public static void saveModeTransactionSync(final List<? extends Model> models) {
+        if (models == null || models.size() == 0) {
+            return;
+        }
+
+        F
+
+        DatabaseDefinition database = FlowManager.getDatabaseForTable(models.get(0).getClass());
+        Transaction transaction = database.beginTransactionAsync(new ITransaction() {
+            @Override
+            public void execute(DatabaseWrapper databaseWrapper) {
+                int count = models.size();
+                for (int index = 0; index < count; index++) {
+                    if (models.get(index).exists()) {
+                        models.get(index).update();
+                    } else {
+                        models.get(index).save();
+                    }
+                }
+            }
+        }).build();
+        transaction.executeSync();
+    }
+
+
+
+    public static void saveModeTransactionSync(final List<? extends Model> models) {
+        if (models == null || models.size() == 0) {
+            return;
+        }
+        DatabaseDefinition database = FlowManager.getDatabaseForTable(models.get(0).getClass());
+        Transaction transaction = database.beginTransactionAsync(new ITransaction() {
+            @Override
+            public void execute(DatabaseWrapper databaseWrapper) {
+                int count = models.size();
+                for (int index = 0; index < count; index++) {
+                    if (models.get(index).exists()) {
+                        models.get(index).update();
+                    } else {
+                        models.get(index).save();
+                    }
+                }
+            }
+        }).build();
+        transaction.executeSync();
+    }
+
     @DebugLog
-    public static void init(Context context, int userId) {
+    public static void init(Context context) {
         context = context.getApplicationContext();
         FlowConfig.Builder flowConfig = new FlowConfig.Builder(context).openDatabasesOnInit(true);
         if (!sIsInit) {
